609c1920276be48e25b19fb2bf9e6bd114523f40
mliq <liquori@gmail.com>
2015-04-30 16:20:51 -0500

    Normal mode complete

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+var cityHtml = null;
 var cityData = null;
 
 $(document).ready(function(){
     $('.initiate').on('click', function(){
-        if(cityData === null){
+        if(cityHtml === null){
             $.get('template.html', function(data){
-               cityData = data;
+               cityHtml = data;
                $('.container').append(data);
             });
         } else {
             console.log("You already initiated.");
         }
-    })
+
+        $.get('data.json', function(data){
+            cityData = data;
+            var cityName = data.cities[0].name;
+            var cityArea = data.cities[0].area;
+            var cityPop = data.cities[0].population;
+            $('.cityDataContainer').append("<p>Name: " + cityName + "<br>Area: " + cityArea + "<br>Population: " + cityPop + "</p>");
+        });
+
+        $('.container').on('click', '.removeButton', function(){
+            //console.log("works", this);
+            $(this).parents('.cityContainer').remove();
+        });
+    });
+
 });
